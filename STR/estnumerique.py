a3aeaedd5450993506a3847f92896aee24eba241
assoudix <assoudiomar@gmail.com>
2024-02-24 16:32:01 +0100

    str

--- STR/estnumerique.py
+++ STR/estnumerique.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def estnumerique(str):
+    for i in str:
+        if i not in "0123456789":
+            return False
+
+    return True
+
+
+if __name__=="__main__":
+    str1='omar 120297'
+    str2='120297'
+    print(estnumerique(str1))
+    print(estnumerique(str2))
